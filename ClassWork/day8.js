b16c16bcf113d9605ca81ad9deab0311e95be3c0
BeckG16 <40575235+BeckG16@users.noreply.github.com>
2018-07-05 12:50:45 -0400

    classwork day 8

---
+++ ClassWork/day8.js
@@ -0,0 +1,8 @@
+function changeParagraph()
+{
+	document.getElementById("demo").innerHTML="Hello Javascript";
+}
+document.addEventListener('DOMContentLoaded', function()
+{
+	changeParagraph();
+});
